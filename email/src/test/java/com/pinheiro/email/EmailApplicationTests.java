2a497481596ed608f0c0285c9e691e5a36ec5f47
Guilherme Alves Pinheiro <guiapinheiro29@gmail.com>
2024-04-18 12:36:17 -0300

    initial configs

---
+++ email/src/test/java/com/pinheiro/email/EmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinheiro.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
