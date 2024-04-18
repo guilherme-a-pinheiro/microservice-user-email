b5b13db261a90c4088d18fa40017c54f1a00124e
Guilherme Alves Pinheiro <guiapinheiro29@gmail.com>
2024-04-18 12:39:52 -0300

    UserModel criado

---
+++ user/src/main/java/com/pinheiro/user/models/UserModel.java
@@ -0,0 +1,41 @@
+package com.pinheiro.user.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_USERS")
+public class UserModel implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String name;
+    private String email;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
