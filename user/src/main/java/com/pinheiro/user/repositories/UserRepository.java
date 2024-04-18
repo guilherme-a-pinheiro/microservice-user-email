94a6b78f7321ec74fe71d7b793ea145de5acfdd0
Guilherme Alves Pinheiro <guiapinheiro29@gmail.com>
2024-04-18 12:41:34 -0300

    UserRepository criado

---
+++ user/src/main/java/com/pinheiro/user/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.pinheiro.user.repositories;
+
+import com.pinheiro.user.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+}
