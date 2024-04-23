af06913a8f475c026731bd010088c39dc14596f7
Guilherme Alves Pinheiro <guiapinheiro29@gmail.com>
2024-04-22 23:27:46 -0300

    envio de email finalizado

--- email/src/main/java/com/pinheiro/email/consumer/EmailConsumer.java
+++ email/src/main/java/com/pinheiro/email/consumer/EmailConsumer.java
@@ -2,18 +2,23 @@ package com.pinheiro.email.consumer;
 
 import com.pinheiro.email.dtos.EmailRecordDTO;
 import com.pinheiro.email.models.EmailModel;
+import com.pinheiro.email.services.EmailService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmailConsumer {
 
+    @Autowired
+    private EmailService service;
+
     @RabbitListener(queues = "${broker.queue.email.name}")
     public void listenEmailQueue(@Payload EmailRecordDTO emailRecordDTO){
         var emailModel = new EmailModel();
         BeanUtils.copyProperties(emailRecordDTO, emailModel);
-
+        service.sendEmail(emailModel);
     }
 }
--- email/src/main/java/com/pinheiro/email/repositories/EmailRepository.java
+++ email/src/main/java/com/pinheiro/email/repositories/EmailRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface EmailRepository extends JpaRepository<UUID, EmailModel> {
+public interface EmailRepository extends JpaRepository<EmailModel, UUID> {
 }
